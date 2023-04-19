80e621ceb71d39cf3f471761e4130aa65444d272
Goon <duy@wearetopgroup.com>
2023-04-19 12:54:56 +0700

    ci(test): print db_uri while testing

--- src/modules/AppDatabase.ts
+++ src/modules/AppDatabase.ts
@@ -7,7 +7,9 @@ import { Config } from "@/app.config";
 
 const dbName = Config.DB_NAME;
 
-export async function connect(onConnected?: (db: typeof mongoose) => void) {
+export async function connect(onConnected?: (db?: typeof mongoose) => void) {
+	console.log("Config.DB_URI :>> ", Config.DB_URI);
+	console.log("Config.DB_NAME :>> ", Config.DB_NAME);
 	try {
 		const db = await mongoose.connect(Config.DB_URI, {
 			dbName,
@@ -20,6 +22,7 @@ export async function connect(onConnected?: (db: typeof mongoose) => void) {
 		if (typeof onConnected != "undefined") onConnected(db);
 	} catch (e) {
 		logError(e);
+		process.exit(1); // passing 1 - will exit the proccess with error
 	}
 }
 
