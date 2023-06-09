cb6cd821b833f204ffb94810e2a5172c04c9ae86
Goon <duy@wearetopgroup.com>
2023-06-09 11:48:04 +0700

    perf(server,api): improve build and deploy api with error message

--- src/modules/build/build-and-deploy.ts
+++ src/modules/build/build-and-deploy.ts
@@ -26,13 +26,13 @@ export const buildAndDeploy = async (buildParams: StartBuildParams, deployParams
 	const { env } = deployParams;
 
 	// [2] Deploy the build to target deploy environment
-	const { release, deployment } = await deployBuild(build, deployParams);
+	const deployRes = await deployBuild(build, deployParams);
+	if (deployRes?.error) throw new Error(`Unable to deploy "${buildParams.appSlug}" app (${buildParams.env}): ${deployRes?.error}`);
+
+	const { release, deployment } = deployRes;
 	console.log("[BUILD_AND_DEPLOY] Finished deploying > release :>> ", release);
 
-	if (!release) throw new Error(`Unable to deploy "${build.slug}" build (${appSlug} - ${buildParams.env}).`);
-
 	const releaseId = MongoDB.toString(release._id);
-
 	const { endpoint, prereleaseUrl } = deployment;
 
 	// [3] Print success information
