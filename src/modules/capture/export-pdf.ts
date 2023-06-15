353319aa5dae77f556d5e5db3feb4dc26dc6b253
Goon <duy@wearetopgroup.com>
2023-06-15 19:07:16 +0700

    feat(server): capture screenshot when build and deploy

--- src/modules/capture/export-pdf.ts
+++ src/modules/capture/export-pdf.ts
@@ -1,4 +1,10 @@
+import { makeDaySlug } from "diginext-utils/dist/string/makeDaySlug";
+import { existsSync, mkdirSync } from "fs";
+import path from "path";
 import puppeteer from "puppeteer";
+
+import { Config } from "@/app.config";
+import { CLI_DIR } from "@/config/const";
 
 export type ExportPDFOptions = {
 	viewport?: {
@@ -75,6 +81,16 @@ const exportPdf = async (url: string, options: ExportPDFOptions = defaultExportP
 
 	const _options = { ...defaultExportPdfOptions, ...options };
 
+	// upload directory
+	const dir = path.resolve(CLI_DIR, "public/upload/pdf");
+	if (!existsSync(dir)) mkdirSync(dir, { recursive: true });
+
+	const fileName = `webpage-${makeDaySlug({ divider: "" })}.pdf`;
+	const filePath = path.resolve(dir, fileName);
+	const fileUrl = `${Config.BASE_URL}/upload/pdf/${fileName}`;
+
+	_options.path = filePath;
+
 	const browser = await puppeteer.launch({
 		headless: "new",
 		defaultViewport: _options.viewport,
@@ -107,7 +123,7 @@ const exportPdf = async (url: string, options: ExportPDFOptions = defaultExportP
 
 	// res.contentType("application/pdf");
 	// res.send(pdfBuffer);
-	return { buffer: pdfBuffer, mime: "application/pdf" };
+	return { name: fileName, path: filePath, url: fileUrl, buffer: pdfBuffer, mime: "application/pdf" };
 };
 
 export default exportPdf;
