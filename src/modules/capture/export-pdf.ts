287206028b06cbef88b098bf894562cd60409cb2
Goon <duy@wearetopgroup.com>
2023-06-15 17:33:01 +0700

    feat(server): add export pdf, capture screenshot webpage

--- src/modules/capture/export-pdf.ts
+++ src/modules/capture/export-pdf.ts
@@ -1,13 +1,51 @@
-import type { PaperFormat, PDFMargin, Viewport } from "puppeteer";
 import puppeteer from "puppeteer";
 
 export type ExportPDFOptions = {
-	viewport?: Viewport;
+	viewport?: {
+		/**
+		 * The page width in pixels.
+		 */
+		width: number;
+		/**
+		 * The page height in pixels.
+		 */
+		height: number;
+		/**
+		 * Specify device scale factor.
+		 * See {@link https://developer.mozilla.org/en-US/docs/Web/API/Window/devicePixelRatio | devicePixelRatio} for more info.
+		 *
+		 * @remarks
+		 * Setting this value to `0` will set the deviceScaleFactor to the system default.
+		 *
+		 * @defaultValue `1`
+		 */
+		deviceScaleFactor?: number;
+		/**
+		 * Whether the `meta viewport` tag is taken into account.
+		 * @defaultValue `false`
+		 */
+		isMobile?: boolean;
+		/**
+		 * Specifies if the viewport is in landscape mode.
+		 * @defaultValue `false`
+		 */
+		isLandscape?: boolean;
+		/**
+		 * Specify if the viewport supports touch events.
+		 * @defaultValue `false`
+		 */
+		hasTouch?: boolean;
+	};
 	printBackground?: boolean;
 	path?: string;
-	format?: PaperFormat;
+	format?: "letter" | "legal" | "tabloid" | "ledger" | "a0" | "a1" | "a2" | "a3" | "a4" | "a5" | "a6";
 	scale?: number;
-	margin?: PDFMargin;
+	margin?: {
+		top?: string | number;
+		bottom?: string | number;
+		left?: string | number;
+		right?: string | number;
+	};
 	displayHeaderFooter?: boolean;
 	/**
 	 * Hides default white background and allows generating pdfs with transparency.
@@ -20,7 +58,7 @@ const defaultExportPdfOptions: ExportPDFOptions = {
 	viewport: { width: 1400, height: 900 },
 	printBackground: true,
 	path: "../public/pdf/webpage.pdf",
-	format: "A4",
+	format: "a4",
 	scale: 1,
 	margin: {
 		top: "20px",
@@ -38,7 +76,7 @@ const exportPdf = async (url: string, options: ExportPDFOptions = defaultExportP
 	const _options = { ...defaultExportPdfOptions, ...options };
 
 	const browser = await puppeteer.launch({
-		headless: true,
+		headless: "new",
 		defaultViewport: _options.viewport,
 		executablePath: process.env.CHROMIUM_PATH,
 		args: ["--no-sandbox"],
@@ -64,6 +102,8 @@ const exportPdf = async (url: string, options: ExportPDFOptions = defaultExportP
 
 	await page.close();
 	await browser.close();
+
+	if (!pdfBuffer) return;
 
 	// res.contentType("application/pdf");
 	// res.send(pdfBuffer);
