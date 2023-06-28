74c0a3383b5b0876f8fc826ffbab2bd94437250b
Tam L <tamlam@wearetopgroup.com>
2023-06-28 18:50:30 +0700

    fix: roll back db when transfer error

--- src/modules/git/initalizeAndCreateDefaultBranches.ts
+++ src/modules/git/initalizeAndCreateDefaultBranches.ts
@@ -1,5 +1,4 @@
 // import { log } from "diginext-utils/dist/xconsole/log";
-import Timer from "diginext-utils/dist/Timer";
 import { log } from "diginext-utils/dist/xconsole/log";
 import * as fs from "fs";
 import path from "path";
@@ -12,56 +11,63 @@ import { makeSlug } from "@/plugins/slug";
 export const initalizeAndCreateDefaultBranches = async (options: InputOptions) => {
 	// console.log("options.username :>> ", options.username);
 
-	// Remove current git & initialize new git...
-	const gitDir = path.resolve(options.targetDirectory, ".git/");
-	if (fs.existsSync(gitDir)) fs.rmSync(gitDir, { recursive: true, force: true });
+	let err = false;
+	try {
+		// Remove current git & initialize new git...
+		const gitDir = path.resolve(options.targetDirectory, ".git/");
+		if (fs.existsSync(gitDir)) fs.rmSync(gitDir, { recursive: true, force: true });
 
-	// Initialize local git...
-	const git = simpleGit(options.targetDirectory, {
-		baseDir: `${options.targetDirectory}`,
-		binary: "git",
-	});
-	await git.init();
+		// Initialize local git...
+		const git = simpleGit(options.targetDirectory, {
+			baseDir: `${options.targetDirectory}`,
+			binary: "git",
+		});
+		await git.init();
 
-	// create main
-	await git.checkout(["-b", "main"]);
+		// create main
+		await git.checkout(["-b", "main"]);
 
-	// make sure the remote git is ready...
-	await wait(1000);
+		// make sure the remote git is ready...
+		await wait(1000);
 
-	// add git origin:
-	await git.addRemote("origin", options.remoteSSH);
+		// add git origin:
+		await git.addRemote("origin", options.remoteSSH);
 
-	// create default brand: "main"
-	await git.fetch(["--all"]);
+		// create default brand: "main"
+		await git.fetch(["--all"]);
 
-	// add git origin:
-	await git.addRemote("origin", options.remoteSSH);
+		// add git origin:
+		await git.addRemote("origin", options.remoteSSH);
 
-	// stage all deployment files & commit it
-	await git.add(".");
-	await git.commit("feat(initial): initial commit");
+		// stage all deployment files & commit it
+		await git.add(".");
+		await git.commit("feat(initial): initial commit");
 
-	{
-		//debug
-		const branch = await git.branch();
-		if (options.isDebugging) log("branch.current :>> ", branch?.current);
+		{
+			//debug
+			const branch = await git.branch();
+			if (options.isDebugging) log("branch.current :>> ", branch?.current);
 
-		const remote = await git.remote(["-v"]);
-		if (options.isDebugging) log("remote :>> ", remote);
+			const remote = await git.remote(["-v"]);
+			if (options.isDebugging) log("remote :>> ", remote);
+		}
+
+		await git.push(["--set-upstream", "origin", "main"]);
+
+		// create developer branches
+		const gitUsername = (await git.getConfig(`user.name`, "global")).value;
+		const username = options.username || (gitUsername ? makeSlug(gitUsername).toLowerCase() : undefined) || "developer";
+		const devBranch = `dev/${username}`;
+		await git.checkout(["-b", devBranch]);
+		await git.push(["--set-upstream", "origin", devBranch]);
+
+		// log(`Finished initializing git!`);
+
+		return options;
+	} catch (error) {
+		console.error(`initalizeAndCreateDefaultBranches error`, error);
+		err = true;
 	}
 
-	await Timer.wait(100);
-	await git.push(["-u", "origin", "main"]);
-
-	// create developer branches
-	const gitUsername = (await git.getConfig(`user.name`, "global")).value;
-	const username = options.username || (gitUsername ? makeSlug(gitUsername).toLowerCase() : undefined) || "developer";
-	const devBranch = `dev/${username}`;
-	await git.checkout(["-b", devBranch]);
-	await git.push("origin", devBranch);
-
-	// log(`Finished initializing git!`);
-
-	return options;
+	return err;
 };
