7e01852805c0b9d0bbd249e9c7353f484b0ccf78
Tam L <tamlam@wearetopgroup.com>
2023-07-06 18:08:44 +0700

    feat(cli): initalizeAndCreateDefaultBranches > Update main branch protection

--- src/modules/git/initalizeAndCreateDefaultBranches.ts
+++ src/modules/git/initalizeAndCreateDefaultBranches.ts
@@ -1,4 +1,5 @@
 // import { log } from "diginext-utils/dist/xconsole/log";
+import axios from "axios";
 import { log } from "diginext-utils/dist/xconsole/log";
 import * as fs from "fs";
 import path from "path";
@@ -50,6 +51,38 @@ export const initalizeAndCreateDefaultBranches = async (options: InputOptions) =
 		}
 
 		await git.push(["--set-upstream", "origin", "main"]);
+
+		// Update main branch protection
+		{
+			const token = options.git.access_token;
+			const owner = options.git.owner;
+			const repo = options.repoSlug;
+			const branch = "main";
+
+			try {
+				const res = await axios({
+					method: "put",
+					url: `https://api.github.com/repos/${owner}/${repo}/branches/${branch}/protection`,
+					headers: {
+						Accept: "application/vnd.github.v3+json",
+						Authorization: `token ${token}`,
+					},
+					data: {
+						required_status_checks: {
+							strict: true,
+							contexts: ["continuous-integration/travis-ci"],
+						},
+						enforce_admins: true,
+						required_pull_request_reviews: null,
+						restrictions: null,
+					},
+				});
+				// if (options.isDebugging)
+				log("res Update main branch protection :>> ", res);
+			} catch (error) {
+				console.error(`Update main branch protection error`, error);
+			}
+		}
 
 		// create developer branches
 		const gitUsername = (await git.getConfig(`user.name`, "global")).value;
