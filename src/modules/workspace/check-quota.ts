e8340d4af7e4ae9fee03cd4f4d524f3123055c3e
Goon <duy@wearetopgroup.com>
2024-11-10 14:43:28 +0700

    fix(server): add super account

--- src/modules/workspace/check-quota.ts
+++ src/modules/workspace/check-quota.ts
@@ -1,24 +1,30 @@
 import { toNumber } from "lodash";
 
-import { IsTest } from "@/app.config";
+import { IsSuperAccount, IsTest } from "@/app.config";
 import type { IWorkspace } from "@/entities";
 import type { ResourceQuotaSize } from "@/interfaces/SystemTypes";
+import { AppService } from "@/services/AppService";
+import { BuildService } from "@/services/BuildService";
+import { ProjectService } from "@/services/ProjectService";
+import { WorkspaceService } from "@/services/WorkspaceService";
 
 import type { CheckQuotaParams, CheckQuotaResponse } from "../diginext/dx-subscription";
 import { dxCheckQuota } from "../diginext/dx-subscription";
 
 export async function checkQuota(workspace: IWorkspace, options: { resourceSize?: ResourceQuotaSize; isDebugging?: boolean } = {}) {
-	const { DB } = await import("../api/DB");
-
 	// SKIP on development and test environment
-	if (IsTest()) return { status: 1, data: { isExceed: false }, messages: ["Ok"] } as CheckQuotaResponse;
+	if (IsTest() || IsSuperAccount(workspace.slug)) return { status: 1, data: { isExceed: false }, messages: ["Ok"] } as CheckQuotaResponse;
 
 	const { dx_key } = workspace;
 	const { resourceSize } = options;
 
-	const projects = await DB.count("project", { workspace: workspace._id });
-	const apps = await DB.count("app", { workspace: workspace._id });
-	const concurrentBuilds = await DB.count("build", { status: "building", workspace: workspace._id });
+	const projectSvc = new ProjectService();
+	const appSvc = new AppService();
+	const buildSvc = new BuildService();
+
+	const projects = await projectSvc.count({ workspace: workspace._id });
+	const apps = await appSvc.count({ workspace: workspace._id });
+	const concurrentBuilds = await buildSvc.count({ status: "building", workspace: workspace._id });
 
 	let containerSize: number;
 	if (resourceSize) containerSize = resourceSize === "none" ? 0 : toNumber(resourceSize.substring(0, resourceSize.length - 1));
@@ -32,9 +38,8 @@ export async function checkQuota(workspace: IWorkspace, options: { resourceSize?
 }
 
 export async function checkQuotaByWorkspaceId(id: string) {
-	const { DB } = await import("../api/DB");
-
-	const workspace = await DB.findOne("workspace", { _id: id });
+	const workspaceSvc = new WorkspaceService();
+	const workspace = await workspaceSvc.findOne({ _id: id });
 	if (!workspace) throw new Error(`Workspace not found`);
 
 	return checkQuota(workspace);
