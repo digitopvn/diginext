04d43e9178737555086e9675fb82214dae514b34
Goon <duy@wearetopgroup.com>
2025-03-25 18:00:53 +0700

    fix(preprocess-input-data): update input data processing logic

--- src/plugins/preprocess-input-data.ts
+++ src/plugins/preprocess-input-data.ts
@@ -1,6 +1,6 @@
-import { isBooleanString, isJSON, isNumberString } from "class-validator";
+import { isBooleanString, isJSON } from "class-validator";
 import { toBool } from "diginext-utils/dist/object";
-import { cloneDeepWith, toNumber, toString } from "lodash";
+import { cloneDeepWith } from "lodash";
 import { isValidObjectId } from "mongoose";
 
 import { isObjectId, MongoDB, toObjectId } from "./mongodb";
@@ -9,7 +9,7 @@ export function preprocessInputData(data: any) {
 	const processedData = cloneDeepWith(data, function (val) {
 		if (isValidObjectId(val)) return MongoDB.toString(toObjectId(val));
 		if (isObjectId(val)) return MongoDB.toString(val);
-		if (isNumberString(val)) return val.toString().length < 12 ? toNumber(val) : toString(val);
+		// if (isNumberString(val)) return val.toString().length < 12 ? toNumber(val) : toString(val);
 		if (isBooleanString(val)) return toBool(val);
 		if (isJSON(val)) return JSON.parse(val);
 		if (val === "undefined" || val === "null") return null;
